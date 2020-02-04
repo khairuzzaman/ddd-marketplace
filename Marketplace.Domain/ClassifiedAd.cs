a391b298aeac606c28ec7d759eab6f3ce64ca4a0
Abu Zafor Khairuzzaman <azkhairuzzaman@gmail.com>
2020-02-04 22:30:41 +0600

    Add classified add entity

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; private set; }
+
+        public ClassifiedAd(Guid id)
+        {
+            if(id == default)
+            {
+                throw new ArgumentException("Identity must be specified", nameof(id));
+            }
+            Id = id;
+        }
+
+        private Guid _ownerId;
+        private string _title;
+        private string _text;
+        private decimal _price;
+    }
+}
