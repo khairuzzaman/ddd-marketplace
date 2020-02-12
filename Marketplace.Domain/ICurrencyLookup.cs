c48802d5391f36442435f5991f93e78a052f4ec3
Abu Zafor Khairuzzaman <zafor@aplectrum.com>
2020-02-12 17:52:25 +0600

    Add domian service

---
+++ Marketplace.Domain/ICurrencyLookup.cs
@@ -0,0 +1,21 @@
+﻿using Marketplace.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.Domain
+{
+    public interface ICurrencyLookup
+    {
+        CurrencyDetails FindCurrency(string currencyCode);
+    }
+
+    public class CurrencyDetails : Value<CurrencyDetails>
+    {
+        public string CurrencyCode { get; set; }
+        public bool InUse { get; set; }
+        public int DecimalPlaces { get; set; }
+
+        public static CurrencyDetails None = new CurrencyDetails { InUse = false };
+    }
+}
